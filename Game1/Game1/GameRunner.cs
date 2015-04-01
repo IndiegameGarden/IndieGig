fbb8cef1f5c575903244407c688106ac5133113b
IndiegameGarden <info@indiegamegarden.com>
2015-04-01 23:06:42 +0200

    fix in delayed state change

--- Game1/Game1/GameRunner.cs
+++ Game1/Game1/GameRunner.cs
@@ -60,7 +60,7 @@ namespace Game1
                 afc.IsFading = true;
 
                 // delayed state change
-                int dlyMs = (int)(1000 * Game1.BACKGROUND_STAR_ROTATION_SPEED / Game1.BACKGROUND_ROTATION_SLOWDOWN_SPEED) + 100;
+                int dlyMs = (int)(1000 * (ac.Ampl-afc.FadeTarget)/afc.FadeSpeed) + 150;
                 var stateChangeTask = new ThreadedTask(new DelayedStateChanger(dlyMs, Game1.GlobalStateEnum.STATE_PLAYING_PHASE1));
                 stateChangeTask.Start();
             }
